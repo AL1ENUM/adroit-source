1f8abfecf8ba44aad4621a0fc94709a2b707830d
alienum <67610662+AL1ENUM@users.noreply.github.com>
2021-01-30 13:13:30 +0200

    Create R.java

---
+++ AdroitServer/src/R.java
@@ -0,0 +1,71 @@
+package adroit;
+
+import java.io.Serializable;
+
+public class R implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String username;
+	String password;
+	String option;
+	Idea idea;
+	
+	public R() {
+		
+	}
+	
+	public R(String username, String password, String option, Idea idea) {
+
+		this.username = username;
+		this.password = password;
+		this.option = option;
+		this.idea = idea;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getOption() {
+		return option;
+	}
+
+	public void setOption(String option) {
+		this.option = option;
+	}
+
+	public Idea getIdea() {
+		return idea;
+	}
+
+	public void setIdea(Idea idea) {
+		this.idea = idea;
+	}
+
+	@Override
+	public String toString() {
+		return "R [username=" + username + ", password=" + password + ", option=" + option + ", idea=" + idea + "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
